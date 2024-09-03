3b7649194487c9261c9beca54541f81aac647789
CONAIPD <159671095+conaipd@users.noreply.github.com>
2024-09-03 16:11:37 -0600

    Actualizacion

---
+++ resources/views/a/c13.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('title', 'Inicio')
+
+@section('content')
+<div class="container">
+    <h2>Nivel A > Criterio 1: Contenido no textual</h2>
+	<ul class="nav nav-tabs nav-fill nav-pills" id="myTab" role="tablist">
+        <li class="nav-item">
+            <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Opción A</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Opción B</a>
+        </li>
+    </ul>
+    <div class="tab-content my-4" id="myTabContent">
+        <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+            <img src="{{ asset('images/c1.jpg') }}" alt="Imagen" class="img-fluid">
+        </div>
+        <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+            <img src="{{ asset('images/c1.jpg') }}" alt="Imagen" class="img-fluid">
+            <div class="alert alert-dark mt-2" role="alert">
+				Se muestra una imagen con 2 niños sobre un barco de madera, navegando por un río que tiene 3 patos y rocas a la orilla, fuera del río hay pasto, muchos árboles y rocas, también 2 cabras que están viendo a los niños en su bote.
+			</div>
+        </div>
+    </div>
+</div>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,123 +10,112 @@
     <!-- Agrega otros enlaces a CSS aquí si es necesario -->
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Accesibilidad</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownA" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Nivel A
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdownA">
+                <li><a class="dropdown-item" href="{{ url('/a/c1') }}">Criterio 1</a></li>
+                <li><a class="dropdown-item" href="{{ url('/a/c2') }}">Criterio 2</a></li>
+                <li><a class="dropdown-item" href="{{ url('/a/c3') }}">Criterio 3</a></li>
+                <li><a class="dropdown-item" href="{{ url('/a/c4') }}">Criterio 4</a></li>
+                <li><a class="dropdown-item" href="{{ url('/a/c5') }}">Criterio 5</a></li>
+                <li><a class="dropdown-item" href="{{ url('/a/c6') }}">Criterio 6</a></li>
+                <li><a class="dropdown-item" href="{{ url('/a/c7') }}">Criterio 7</a></li>
+                <li><a class="dropdown-item" href="{{ url('/a/c8') }}">Criterio 8</a></li>
+                <li><a class="dropdown-item" href="{{ url('/a/c9') }}">Criterio 9</a></li>
+                <li><a class="dropdown-item" href="{{ url('/a/c10') }}">Criterio 10</a></li>
+                <li><a class="dropdown-item" href="{{ url('/a/c11') }}">Criterio 11</a></li>
+                <li><a class="dropdown-item" href="{{ url('/a/c12') }}">Criterio 12</a></li>
+                <li><a class="dropdown-item" href="{{ url('/a/c13') }}">Criterio 13</a></li>
+                <li><a class="dropdown-item" href="{{ url('/a/c14') }}">Criterio 14</a></li>
+                <li><a class="dropdown-item" href="{{ url('/a/c15') }}">Criterio 15</a></li>
+                <li><a class="dropdown-item" href="{{ url('/a/c16') }}">Criterio 16</a></li>
+                <li><a class="dropdown-item" href="{{ url('/a/c17') }}">Criterio 17</a></li>
+                <li><a class="dropdown-item" href="{{ url('/a/c18') }}">Criterio 18</a></li>
+                <li><a class="dropdown-item" href="{{ url('/a/c19') }}">Criterio 19</a></li>
+                <li><a class="dropdown-item" href="{{ url('/a/c20') }}">Criterio 20</a></li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownAA" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Nivel AA
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdownAA">
+                <li><a class="dropdown-item" href="{{ url('/aa/c1') }}">Criterio 1</a></li>
+                <li><a class="dropdown-item" href="{{ url('/aa/c2') }}">Criterio 2</a></li>
+                <li><a class="dropdown-item" href="{{ url('/aa/c3') }}">Criterio 3</a></li>
+                <li><a class="dropdown-item" href="{{ url('/aa/c4') }}">Criterio 4</a></li>
+                <li><a class="dropdown-item" href="{{ url('/aa/c5') }}">Criterio 5</a></li>
+                <li><a class="dropdown-item" href="{{ url('/aa/c6') }}">Criterio 6</a></li>
+                <li><a class="dropdown-item" href="{{ url('/aa/c7') }}">Criterio 7</a></li>
+                <li><a class="dropdown-item" href="{{ url('/aa/c8') }}">Criterio 8</a></li>
+                <li><a class="dropdown-item" href="{{ url('/aa/c9') }}">Criterio 9</a></li>
+                <li><a class="dropdown-item" href="{{ url('/aa/c10') }}">Criterio 10</a></li>
+                <li><a class="dropdown-item" href="{{ url('/aa/c11') }}">Criterio 11</a></li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownAAA" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Nivel AAA
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdownAAA">
+                <li><a class="dropdown-item" href="{{ url('/aaa/c1') }}">Criterio 1</a></li>
+                <li><a class="dropdown-item" href="{{ url('/aaa/c2') }}">Criterio 2</a></li>
+                <li><a class="dropdown-item" href="{{ url('/aaa/c3') }}">Criterio 3</a></li>
+                <li><a class="dropdown-item" href="{{ url('/aaa/c4') }}">Criterio 4</a></li>
+                <li><a class="dropdown-item" href="{{ url('/aaa/c5') }}">Criterio 5</a></li>
+                <li><a class="dropdown-item" href="{{ url('/aaa/c6') }}">Criterio 6</a></li>
+                <li><a class="dropdown-item" href="{{ url('/aaa/c7') }}">Criterio 7</a></li>
+                <li><a class="dropdown-item" href="{{ url('/aaa/c8') }}">Criterio 8</a></li>
+                <li><a class="dropdown-item" href="{{ url('/aaa/c9') }}">Criterio 9</a></li>
+                <li><a class="dropdown-item" href="{{ url('/aaa/c10') }}">Criterio 10</a></li>
+                <li><a class="dropdown-item" href="{{ url('/aaa/c11') }}">Criterio 11</a></li>
+                <li><a class="dropdown-item" href="{{ url('/aaa/c12') }}">Criterio 12</a></li>
+                <li><a class="dropdown-item" href="{{ url('/aaa/c13') }}">Criterio 13</a></li>
+                <li><a class="dropdown-item" href="{{ url('/aaa/c14') }}">Criterio 14</a></li>
+                <li><a class="dropdown-item" href="{{ url('/aaa/c15') }}">Criterio 15</a></li>
+                <li><a class="dropdown-item" href="{{ url('/aaa/c16') }}">Criterio 16</a></li>
+                <li><a class="dropdown-item" href="{{ url('/aaa/c17') }}">Criterio 17</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
     <div class="container-fluid">
         <!-- Topbar -->
-        <header class="navbar navbar-expand navbar-light bg-light fixed-top">
-            <a class="navbar-brand" href="#">Mi Aplicación</a>
-            <ul class="navbar-nav ms-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">
-                        <i class="bi bi-bell"></i>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">
-                        <i class="bi bi-person"></i>
-                    </a>
-                </li>
-            </ul>
-        </header>
+        
 
         <div class="row">
             <!-- Menú Lateral -->
-            <nav id="sidebar" class="col-md-3 col-lg-2 d-md-block bg-light sidebar">
-                <div class="position-sticky">
-                    <ul class="nav flex-column">
-                        <li class="nav-item">
-                            <a class="nav-link active" href="#">
-                                <i class="bi bi-house-door"></i> Dashboard
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/a/c1') }}">
-                                <i class="bi bi-gear"></i> Criterio 1
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/a/c2') }}">
-                                <i class="bi bi-gear"></i> Criterio 2
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/a/c3') }}">
-                                <i class="bi bi-gear"></i> Criterio 3
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/a/c4') }}">
-                                <i class="bi bi-gear"></i> Criterio 4
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/a/c5') }}">
-                                <i class="bi bi-gear"></i> Criterio 5
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/a/c6') }}">
-                                <i class="bi bi-gear"></i> Criterio 6
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/a/c7') }}">
-                                <i class="bi bi-gear"></i> Criterio 7
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/a/c8') }}">
-                                <i class="bi bi-gear"></i> Criterio 8
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/a/c9') }}">
-                                <i class="bi bi-gear"></i> Criterio 9
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/a/c10') }}">
-                                <i class="bi bi-gear"></i> Criterio 10
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/a/c11') }}">
-                                <i class="bi bi-gear"></i> Criterio 11
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/a/c12') }}">
-                                <i class="bi bi-gear"></i> Criterio 12
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/a/c13') }}">
-                                <i class="bi bi-gear"></i> Criterio 13
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/a/c14') }}">
-                                <i class="bi bi-gear"></i> Criterio 14
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/a/c15') }}">
-                                <i class="bi bi-gear"></i> Criterio 15
-                            </a>
-                        </li>
-
-                        <!-- Agrega más ítems de menú aquí -->
-                    </ul>
-                </div>
-            </nav>
 
             <!-- Contenido Principal -->
-            <main role="main" class="col-md-9 ms-sm-auto col-lg-10 px-4">
-                @yield('content')
+            <main role="main" class="col-md-12" style="margin-bottom: 40px;">
+                <div class="row">
+                    <div class="col-lg-2"></div>
+                    <div class="col-lg-8" align="center">
+                        @yield('content')
+                    </div>
+                    <div class="col-lg-2"></div>
             </main>
         </div>
     </div>
 
     <script src="{{ asset('js/app.js') }}"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
